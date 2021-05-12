711acc4fc4735ccb5bfcc948bc1ef5c1e838c32e
kisoo6203 <kisoo6203@naver.com>
2021-05-12 16:41:05 +0900

    05-12 원데이 보충해주시는날

---
+++ Server_09_Diet/src/main/java/com/callor/diet/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.callor.diet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class HomeController extends HttpServlet{
+
+	private static final long serialVersionUID = -953531494441799347L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 최초접속시 보여주는 화면 설정하기
+//		req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
+		
+		ReqController.forward(req, resp, "home");
+		
+	}
+	
+	
+
+}
